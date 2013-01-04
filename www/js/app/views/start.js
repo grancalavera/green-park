2aabf374974944726e71a7508a54c97875b17f8d
Leon Coto <leoncoto@gmail.com>
2013-01-04 18:32:27 +0000

    Canvases ready for drawing!

--- www/js/app/views/start.js
+++ www/js/app/views/start.js
@@ -1,11 +1,68 @@
-// Tunnel section implementation
+// Start section implementation
 // leoncoto@gmail.com
 define(function (require) {
+
     'use strict';
+
     var $ = require('jquery');
+    var _ = require('underscore');
     var Section = require('app/views/section');
+
+    var max = 280;
+    var cols = 4;
+
+    //--------------------------------------------------------------------------
+    //
+    // Start
+    //
+    //--------------------------------------------------------------------------
+
     return Section.extend({
+
+      //----------------------------------
+      //
+      // Setup
+      //
+      //----------------------------------
+
       template: require('templates').start,
-      viewId: 'app/views/start'
+      viewId: 'app/views/start',
+      className: 'start',
+
+      //----------------------------------
+      //
+      // Start stuff
+      //
+      //----------------------------------
+
+      renderingContext: function () {
+        var width = Math.max(this.dimensions.width / cols, max);
+        var additions = {
+          width: width
+        };
+        var defaultContext = Section.prototype.renderingContext.call(this);
+        return _.extend({}, defaultContext, additions);
+      },
+
+      draw: function () {
+        var canvases = this.$('canvas');
+        this.drawSide(canvases[0], 'picadilly');
+        this.drawSide(canvases[1], 'jubilee');
+      },
+
+      drawSide: function (canvas) {
+        var context = canvas.getContext('2d');
+        var margin = 30;
+        if (context) {
+          context.fillStyle = "rgba(200,0,0,0.5)";
+          context.fillRect(
+            margin / 2,
+            margin / 2,
+            this.dimensions.width - margin,
+            this.dimensions.height - margin - 60 // the footer height
+          );
+        }
+      }
+
     });
 });
--- www/js/app/views/tunnel.js
+++ www/js/app/views/tunnel.js
@@ -1,17 +1,46 @@
-// Start section implementation
+// Tunnel section implementation
 // leoncoto@gmail.com
 define(function (require) {
     'use strict';
+
     var $ = require('jquery');
+    var _ = require('underscore');
     var Section = require('app/views/section');
+
+    //--------------------------------------------------------------------------
+    //
+    // Tunnel
+    //
+    //--------------------------------------------------------------------------
+
     return Section.extend({
+
+      //----------------------------------
+      //
+      // Setup
+      //
+      //----------------------------------
+
       template: require('templates').tunnel,
       viewId: 'app/views/tunnel',
       className: 'tunnel',
+
+      //----------------------------------
+      //
+      // Tunnel stuff
+      //
+      //----------------------------------
+
+      renderingContext: function () {
+        var additions = {};
+        var defaultContext = Section.prototype.renderingContext.call(this);
+        return _.extend({}, defaultContext, additions);
+      },
+
       draw: function () {
+        var margin = 30;
         var canvas = this.$('canvas')[0];
         var context = canvas.getContext('2d');
-        var margin = 30;
         if (context) {
           context.fillStyle = "rgba(200,0,0,0.5)";
           context.fillRect(
